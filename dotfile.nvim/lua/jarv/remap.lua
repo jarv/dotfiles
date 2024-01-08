12b3d398b9e37ac2432b9f6b07eff94c71960cf8
John Jarvis <john@jarv.org>
2024-01-08 10:49:47 +0100

    updates remaps

--- dotfile.nvim/lua/jarv/remap.lua
+++ dotfile.nvim/lua/jarv/remap.lua
@@ -1,10 +1,4 @@
-vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<c-l>", vim.cmd.bnext)
 vim.keymap.set("n", "<c-h>", vim.cmd.bprev)
-
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
-
-vim.keymap.set("n", "<leader><leader>", function()
-    vim.cmd("so")
-end)
