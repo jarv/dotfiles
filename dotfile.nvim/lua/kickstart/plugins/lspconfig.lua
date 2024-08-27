2001da11507a3a22fe4818550b8e3dffd6a45ef2
John Jarvis <jarv@gitlab.com>
2024-08-27 21:43:12 +0200

    remove rubocop

--- dotfile.nvim/lua/kickstart/plugins/lspconfig.lua
+++ dotfile.nvim/lua/kickstart/plugins/lspconfig.lua
@@ -171,9 +171,9 @@ return {
 			--  - settings (table): Override the default settings passed when initializing the server.
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
-				rubocop = {
-					cmd = { "bundle", "exec", "rubocop", "--lsp" },
-				},
+				-- rubocop = {
+				-- 	cmd = { "bundle", "exec", "rubocop", "--lsp" },
+				-- },
 				-- clangd = {},
 				-- gopls = {},
 				-- pyright = {},
