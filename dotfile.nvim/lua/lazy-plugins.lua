191880b9d1ea23e56eaa0dd7d92a471369755fc3
John Jarvis <jarv@gitlab.com>
2024-09-08 22:25:17 +0200

    plugin updates

--- dotfile.nvim/lua/lazy-plugins.lua
+++ dotfile.nvim/lua/lazy-plugins.lua
@@ -1,70 +1,29 @@
--- [[ Configure and install plugins ]]
---
---  To check the current status of your plugins, run
---    :Lazy
---
---  You can press `?` in this menu for help. Use `:q` to close the window
---
---  To update plugins you can run
---    :Lazy update
---
--- NOTE: Here is where you install your plugins.
 require("lazy").setup({
-	-- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
 	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
-
 	-- NOTE: Plugins can also be added by using a table,
 	-- with the first argument being the link and the following
 	-- keys can be used to configure plugin behavior/loading/etc.
 	--
 	-- Use `opts = {}` to force a plugin to be loaded.
-	--
-
 	-- modular approach: using `require 'path/name'` will
 	-- include a plugin definition from file lua/path/name.lua
 	require("kickstart/plugins/rose-pine"),
-
 	require("kickstart/plugins/gitsigns"),
-
 	require("kickstart/plugins/telescope"),
-
 	require("kickstart/plugins/lspconfig"),
-
 	require("kickstart/plugins/conform"),
-
 	require("kickstart/plugins/cmp"),
-	-- require("kickstart/plugins/cmp-spell"),
-
 	require("kickstart/plugins/todo-comments"),
-
 	require("kickstart/plugins/mini"),
-
 	require("kickstart/plugins/treesitter"),
 	require("kickstart/plugins/bufferline"),
 	require("kickstart/plugins/vim-fugitive"),
 	require("kickstart/plugins/clipboard-image"),
-
+	require("kickstart/plugins/indent_line"),
+	require("kickstart/plugins/lint"),
+	require("kickstart/plugins/neo-tree"),
+	-- require("kickstart/plugins/debug"),
 	-- require("kickstart/plugins/gitlab-lsp"),
-	-- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
-	-- init.lua. If you want these files, they are in the repository, so you can just download them and
-	-- place them in the correct locations.
-
-	-- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
-	--
-	--  Here are some example plugins that I've included in the Kickstart repository.
-	--  Uncomment any of the lines below to enable them (you will need to restart nvim).
-	--
-	-- require 'kickstart.plugins.debug',
-	-- require 'kickstart.plugins.indent_line',
-	-- require("kickstart.plugins.lint"),
-	-- require 'kickstart.plugins.autopairs',
-	-- require 'kickstart.plugins.neo-tree',
-
-	-- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-	--    This is the easiest way to modularize your config.
-	--
-	--  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-	--    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
 	-- { import = 'custom.plugins' },
 }, {
 	ui = {
