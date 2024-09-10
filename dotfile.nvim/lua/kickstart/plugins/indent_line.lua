893cab21057779f73df7c3424370f86880844390
John Jarvis <john@jarv.org>
2024-09-10 12:54:51 +0200

    removes the indent char

--- dotfile.nvim/lua/kickstart/plugins/indent_line.lua
+++ dotfile.nvim/lua/kickstart/plugins/indent_line.lua
@@ -1,9 +1,15 @@
 return {
-  { -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
-    main = 'ibl',
-    opts = {},
-  },
+	{ -- Add indentation guides even on blank lines
+		"lukas-reineke/indent-blankline.nvim",
+		-- Enable `lukas-reineke/indent-blankline.nvim`
+		-- See `:help ibl`
+		main = "ibl",
+		config = function()
+			require("ibl").setup({
+				indent = { char = "" },
+			})
+		end,
+		opts = {},
+	},
 }
+-- vim: ts=2 sts=2 sw=2 et
