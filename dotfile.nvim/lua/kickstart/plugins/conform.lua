ecaac5dde791456a5b0a7348cf80a5b0e22cdbd2
John Jarvis <john@jarv.org>
2025-01-10 11:21:12 +0100

    aligns nvim config with latest kickstart modular

--- dotfile.nvim/lua/kickstart/plugins/conform.lua
+++ dotfile.nvim/lua/kickstart/plugins/conform.lua
@@ -28,6 +28,8 @@ return {
 			formatters_by_ft = {
 				lua = { "stylua" },
 				javascript = { "prettierd", "prettier", stop_after_first = true },
+				sh = { "shfmt" },
+				bash = { "shfmt" },
 				-- Conform can also run multiple formatters sequentially
 				-- python = { "isort", "black" },
 				--
--- dotfile.nvim/lua/lazy-plugins.lua
+++ dotfile.nvim/lua/lazy-plugins.lua
@@ -1,50 +1,62 @@
-require("lazy").setup({
-	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
-	-- NOTE: Plugins can also be added by using a table,
-	-- with the first argument being the link and the following
-	-- keys can be used to configure plugin behavior/loading/etc.
-	--
-	-- Use `opts = {}` to force a plugin to be loaded.
-	-- modular approach: using `require 'path/name'` will
-	-- include a plugin definition from file lua/path/name.lua
-	require("kickstart/plugins/rose-pine"),
-	require("kickstart/plugins/gitsigns"),
-	require("kickstart/plugins/telescope"),
-	require("kickstart/plugins/lspconfig"),
-	require("kickstart/plugins/conform"),
-	require("kickstart/plugins/cmp"),
-	require("kickstart/plugins/todo-comments"),
-	require("kickstart/plugins/mini"),
-	require("kickstart/plugins/treesitter"),
-	require("kickstart/plugins/bufferline"),
-	require("kickstart/plugins/vim-fugitive"),
-	require("kickstart/plugins/clipboard-image"),
-	require("kickstart/plugins/indent_line"),
-	require("kickstart/plugins/lint"),
-	require("kickstart/plugins/neo-tree"),
-	-- require("kickstart/plugins/debug"),
-	-- require("kickstart/plugins/gitlab-lsp"),
-	-- { import = 'custom.plugins' },
+-- [[ Configure and install plugins ]]
+--
+--  To check the current status of your plugins, run
+--    :Lazy
+--
+--  You can press `?` in this menu for help. Use `:q` to close the window
+--
+--  To update plugins you can run
+--    :Lazy update
+--
+-- NOTE: Here is where you install your plugins.
+require('lazy').setup({
+  -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  -- NOTE: Plugins can also be added by using a table,
+  -- with the first argument being the link and the following
+  -- keys can be used to configure plugin behavior/loading/etc.
+  --
+  -- Use `opts = {}` to force a plugin to be loaded.
+  -- modular approach: using `require 'path/name'` will
+  -- include a plugin definition from file lua/path/name.lua
+
+  require 'kickstart/plugins/gitsigns',
+  require 'kickstart/plugins/telescope',
+  require 'kickstart/plugins/lspconfig',
+  require 'kickstart/plugins/conform',
+  require 'kickstart/plugins/cmp',
+  require 'kickstart/plugins/todo-comments',
+  require 'kickstart/plugins/mini',
+  require 'kickstart/plugins/treesitter',
+  require 'kickstart/plugins/bufferline',
+  require 'kickstart/plugins/vim-fugitive',
+  require 'kickstart/plugins/clipboard-image',
+  require 'kickstart/plugins/indent_line',
+  require 'kickstart/plugins/lint',
+  require 'kickstart/plugins/neo-tree',
+  -- require("kickstart/plugins/debug"),
+  -- require("kickstart/plugins/gitlab-lsp"),
+  -- { import = 'custom.plugins' },
 }, {
-	ui = {
-		-- If you are using a Nerd Font: set icons to an empty table which will use the
-		-- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
-		icons = vim.g.have_nerd_font and {} or {
-			cmd = "⌘",
-			config = "🛠",
-			event = "📅",
-			ft = "📂",
-			init = "⚙",
-			keys = "🗝",
-			plugin = "🔌",
-			runtime = "💻",
-			require = "🌙",
-			source = "📄",
-			start = "🚀",
-			task = "📌",
-			lazy = "💤 ",
-		},
-	},
+  ui = {
+    -- If you are using a Nerd Font: set icons to an empty table which will use the
+    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = '⌘',
+      config = '🛠',
+      event = '📅',
+      ft = '📂',
+      init = '⚙',
+      keys = '🗝',
+      plugin = '🔌',
+      runtime = '💻',
+      require = '🌙',
+      source = '📄',
+      start = '🚀',
+      task = '📌',
+      lazy = '💤 ',
+    },
+  },
 })
 
 -- vim: ts=2 sts=2 sw=2 et
